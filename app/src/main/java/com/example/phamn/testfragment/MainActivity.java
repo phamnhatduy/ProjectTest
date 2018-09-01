68b3b0ba8a0e9ce4bbfea6603af13166c38e688d
phamnhatduy <phamnhatduy1997@gmail.com>
2018-09-01 21:33:23 +0700

    test thử lần 1

---
+++ app/src/main/java/com/example/phamn/testfragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.phamn.testfragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentA fragmentA = new FragmentA();
+        fragmentTransaction.add(R.id.frameContent, fragmentA);
+        fragmentTransaction.commit();
+    }
+}
